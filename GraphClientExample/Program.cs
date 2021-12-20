4e0ce53fd5a77036c985b12beaa373906b80107e
matt liu <mliu@xignite.com>
2021-12-20 10:46:39 +0800

    update main function.

--- GraphClientExample/Program.cs
+++ GraphClientExample/Program.cs
@@ -7,7 +7,25 @@ namespace GraphClientExample
     {
         private static void Main(string[] args)
         {
+            connection = null;
+            try
+            {
+                connection = getConnection();
+                AuthResult authResult = connection.authenticate(userName, password);
+                return new Session(connection, authResult, this, reconnect);
+            }
+            catch (Exception e)
+            {
+                // if get the connection succeeded, but authenticate failed,
+                // needs to return connection to pool
+                if (connection != null)
+                {
+                    setInvalidateConnection(connection);
+                }
+                throw e;
+            }
             Session session;
+            session =
             NebulaPool pool = new NebulaPool();
             Console.WriteLine("Hello World!");
         }
