801718a2276051224e666a41e949f1d18248162c
shyboylpf <mliu@xignite.com>
2021-12-20 11:20:47 +0800

    add a simple example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Thrift;
+using Thrift.Transport.Client;
+using Thrift.Protocol;
+using Nebula.Graph;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class GraphClient
+    {
+        private TSocketTransport socketTransport { get; set; }
+        private TBinaryProtocol protocolProtocol { get; set; }
+
+        public GraphClient(string ip, int port)
+        {
+            var tConfiguration = new TConfiguration();
+            socketTransport = new TSocketTransport(ip, port, tConfiguration);
+            //var transport2 = new TMemoryBufferTransport(tConfiguration);
+
+            socketTransport.OpenAsync(default);
+
+            protocolProtocol = new TBinaryProtocol(socketTransport);
+
+            var client = new GraphService.Client(protocolProtocol);
+        }
+    }
+}
--- GraphClientExample/Program.cs
+++ GraphClientExample/Program.cs
@@ -1,5 +1,4 @@
-﻿using Nebula.Meta;
-using System;
+﻿using System;
 
 namespace GraphClientExample
 {
@@ -7,26 +6,6 @@ namespace GraphClientExample
     {
         private static void Main(string[] args)
         {
-            connection = null;
-            try
-            {
-                connection = getConnection();
-                AuthResult authResult = connection.authenticate(userName, password);
-                return new Session(connection, authResult, this, reconnect);
-            }
-            catch (Exception e)
-            {
-                // if get the connection succeeded, but authenticate failed,
-                // needs to return connection to pool
-                if (connection != null)
-                {
-                    setInvalidateConnection(connection);
-                }
-                throw e;
-            }
-            Session session;
-            session =
-            NebulaPool pool = new NebulaPool();
             Console.WriteLine("Hello World!");
         }
     }
