cf8da48ff482f386281d40e19cc4df62f8f7ebfe
zhouyuguangsc <41465309+zhouyuguangsc@users.noreply.github.com>
2022-03-03 19:15:29 +0800

    Update UnitTest1.cs

--- NebulaNetTest/UnitTest1.cs
+++ NebulaNetTest/UnitTest1.cs
@@ -30,8 +30,8 @@ namespace NebulaNetTest
         {
             Assert.IsTrue(Session_id > 0);
             
-            await _client.ExecuteAsync(Session_id,"ADD HOSTS 127.0.0.1:9669;");
-            await Task.Delay(30000);
+            await _client.ExecuteAsync(Session_id,"ADD HOSTS 127.0.0.1:9779;");
+            await Task.Delay(20000);
 
             StringBuilder sb = new StringBuilder();
             sb.Append("CREATE SPACE IF NOT EXISTS test(vid_type=FIXED_STRING(30));");
